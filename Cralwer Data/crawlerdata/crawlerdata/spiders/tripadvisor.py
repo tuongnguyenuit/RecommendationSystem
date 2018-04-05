1440a86967eed64dbba0ee695e84e266d7d9686e
Nguyen Ngoc Tuong (CME.MSA) <TuongNN1@fsoft.com.vn>
2018-04-05 15:18:41 +0700

    update file crawl

--- Cralwer Data/crawlerdata/crawlerdata/spiders/tripadvisor.py
+++ Cralwer Data/crawlerdata/crawlerdata/spiders/tripadvisor.py
@@ -2,15 +2,31 @@ import scrapy
 from scrapy import Spider
 from crawlerdata.items import CrawlerdataItem
 
+
 class TripadvisorSpider(Spider):
-    data = {"binh dinh":[]}
     name = 'tripadvisor'
     allowed_domains = ["tripadvisor.com.vn"]
-    start_urls = [
-        "https://www.tripadvisor.com.vn/Attractions-g608528-Activities-Quy_Nhon_Binh_Dinh_Province.html"
-    ]
+    def start_requests(self):
+        list_url = [
+            # 'https://www.tripadvisor.com.vn/Attractions-g608528-Activities-Quy_Nhon_Binh_Dinh_Province.html',
+            # 'https://www.tripadvisor.com.vn/Attractions-g303946-Activities-Vung_Tau_Ba_Ria_Vung_Tau_Province.html',
+            # "https://www.tripadvisor.com.vn/Attractions-g293925-Activities-Ho_Chi_Minh_City.html",
+            "https://www.tripadvisor.com.vn/Attractions-g293925-Activities-c42-Ho_Chi_Minh_City.html#FILTERED_LIST"
+
+        ]
+        for url in list_url:
+            yield scrapy.Request(url, callback=self.parse)
+
 
     def parse(self, response):
+        # Get url of category
+
+        # Get url of next page
+        for nextpage in response.css('div.al_border.deckTools.btm > div > div > a::attr(href)').extract()[0]:
+            full_url = response.urljoin(nextpage)
+            yield scrapy.Request(full_url)            
+
+        # Get url of element in page
         for element in response.css('div.attraction_element'):
             url = element.css('div.listing_title a::attr(href)').extract()[0]           
             full_url = response.urljoin(url)
@@ -22,7 +38,7 @@ class TripadvisorSpider(Spider):
         item['rank'] = response.css('span.header_popularity.popIndexValidation b span::text').extract_first()
         item['name'] = response.css('h1.heading_title::text').extract_first()
         item['rating'] = response.css('div.rs.rating span::attr(content)').extract_first()
-        item['street_address'] = response.css('span.street-address::text').extract_first() + ', ' + item['location']
+        item['street_address'] = response.css('span.street-address::text').extract_first()
         item['reviews_number'] = response.css('div.rs.rating a.more span::text').extract_first()
         yield item
         
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RecommendationSystem
 Begin with new work - build recommendation system
+
+Here you will find Simple procedures for build recommendation system
